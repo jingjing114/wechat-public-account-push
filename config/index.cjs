d2fc308d515644db12ca7ee21a883b25c7c222ed
jingjing114 <42139902+jingjing114@users.noreply.github.com>
2023-08-25 16:08:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -149,9 +149,9 @@ const USER_CONFIG = {
       //useTemplateId:"KWZt7ZCDkxNZhhEesQdPBDYbwFxgFtdg8197aHjFCjQ",
       useTemplateId:"YUXi9fuFpK7S2UrbVnDJDIii8AL6ipOmi80gFSLrhNM",
        
-      province: "阿坝",
+      province: "四川",
        
-      city: "小金县",
+      city: "成都",
        
       horoscopeDate: '12-25',
        
